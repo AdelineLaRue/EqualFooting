d1255bda6ae0f322bec2fcc1ff75659b1f6b3429
hhh <hhh>
2024-07-29 14:59:00 -0400

    Initial Commit

---
+++ EqualFooting/EqualFootingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EqualFootingApp.swift
+//  EqualFooting
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct EqualFootingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
